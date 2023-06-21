92b5a1c0f9e438839a37577010f82d44995b26b7
Ruthvik Kota <88280622+ruthvikkota@users.noreply.github.com>
2023-06-21 10:26:36 +0530

    Create 35_handle_assignment_copy.sv

---
+++ 35_handle_assignment_copy.sv
@@ -0,0 +1,16 @@
+class packet;
+int a;
+int b;
+endclass
+module copy_method;
+packet p1,p2;
+initial begin
+p1=new();
+p2=p1;
+
+p2.a=10;
+p2.b=20;
+
+$display("p1.a=%0d,p1.b=%0d",p1.a,p1.b);
+end
+endmodule
