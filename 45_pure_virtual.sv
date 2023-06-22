f64cce207c9d957dc7f40a2c1a274f2f54797ee3
Ruthvik Kota <88280622+ruthvikkota@users.noreply.github.com>
2023-06-22 10:21:03 +0530

    Update 45_pure_virtual.sv

--- 45_pure_virtual.sv
+++ 45_pure_virtual.sv
@@ -16,30 +16,18 @@ endfunction
 endclass
 
 
-class xyz extends abstract;
-task display();
-$display("xyz");
-endtask
-endclass
 
 module abstraction;
 abstract ab;
 abc a;
-xyz x;
-int i1,i2;
+int i1;
 initial begin
 //ab=new;
 a=new;
-x=new;
 a.display();
-x.display();
 i1=a.increment(5);
 $display("The value of a.increment=%d",i1);
-i2=x.increment(10);
-$display("The value of x.increment=%d",i2);
 ab=a;
-ab.display();
-ab= x;
 ab.display();
 end
 endmodule
