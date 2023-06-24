e02a856c3a8c5e02c23921620e5a750407772de7
Ruthvik Kota <88280622+ruthvikkota@users.noreply.github.com>
2023-06-24 12:31:01 +0530

    Update 66_constraint_random_example2.sv

--- 66_constraint_random_example2.sv
+++ 66_constraint_random_example2.sv
@@ -1,4 +1,10 @@
-class abc;
+/*
+The array a is '{0, 1, 0, 2, 0, 3, 0, 4, 0, 5, 0} 
+The array a is '{0, 1, 0, 2, 0, 3, 0, 4, 0, 5, 0} 
+The array a is '{0, 1, 0, 2, 0, 3, 0, 4, 0, 5, 0, 6, 0} 
+The array a is '{0, 1, 0, 2, 0, 3, 0, 4, 0, 5, 0, 6, 0, 7} 
+The array a is '{0, 1, 0, 2, 0, 3, 0, 4, 0, 5, 0} 
+*/class abc;
   randc int a[];
   constraint a_size{a.size>10;a.size<15;}
   constraint a_value{foreach(a[i]){if(i%2==0){a[i]==0;}
