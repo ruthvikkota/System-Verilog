56655b3e04a19b3f7f5752ff8dd793ef85653e5a
Ruthvik Kota <88280622+ruthvikkota@users.noreply.github.com>
2023-06-27 12:59:43 +0530

    Update generator.sv

--- Decoder_2x4_SV_testbench/generator.sv
+++ Decoder_2x4_SV_testbench/generator.sv
@@ -11,11 +11,6 @@ repeat(5) begin
 trans=new();
 void'(trans.randomize());
 gen2driv.put(trans);
-$display("-----------");
-$display("In generator class");
-$display("a=%b, b=%b", trans.a, trans.b);
-$display("-----------");
 end
 endtask
 endclass
-
